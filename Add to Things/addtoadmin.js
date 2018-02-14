671e5beeccb789c3fb5911823e8113a220718023
Craig McClellan <craig@craigmcclellan.com>
2018-02-14 12:54:35 -0600

    Added Selection Prompt for Date to Complete

--- Add to Things/addtoadmin.js
+++ Add to Things/addtoadmin.js
@@ -11,43 +11,58 @@ var headings = ["Planning", "Grading", "None"];
 // Split lines of current draft to make them into individual todos
 var lines = draft.content.split("\n");
 
-// Uses the headings listed in the variable headings (above) to make a Prompt to allow you to choose a heading for your Todos.
+// Uses the headings listed in the variable headings (above) to make a Prompt to allow you to choose a heading for and  date to complete your Todos.
 
-function headingChooser() {
+
+function headingAndDate() {
+	var promptResponse = [];
 	var p = Prompt.create();
-	p.title = "Heading?"
+	p.title = "Heading and Date";
 	var arrayLength = headings.length;
 	for (var i = 0; i < arrayLength; i++) {
 		p.addButton(headings[i]);
 	}
+	p.addSelect("when", "Do When?", ["Today", "Tomorrow", "None"], ["Today"], false)
 	var didSelect = p.show();
-	var selection = p.buttonPressed
-	console.log(selection)
-	if (selection == "None") {
-		return ""
+	var headingSelect = p.buttonPressed;
+	if (headingSelect == "None") {
+		promptResponse.push("");
 	}
 	else {
-		return selection
+		promptResponse.push(headingSelect);
 	}
-
+	var dateSelect = p.fieldValues["when"];
+	if (dateSelect == "None") {
+		promptResponse.push("");
+	}
+	else if (dateSelect == "Today") {
+		promptResponse.push("today");
+	}
+	else if (dateSelect == "Tomorrow") {
+		promptResponse.push("tomorrow");
+	}
+	return promptResponse
 }
 
 // Create a list of todos from the separated Draft lines and add them to the project whose ID is listed above.
 
-function todoList(heading) {
+function todoList(headingDate) {
 	var todoArray = [];
+	var date = headingDate[1];
+	var heading = headingDate[0]
 	for (var line of lines) {
 		var todo = TJSTodo.create();
 		todo.title = line;
 		todo.listID = projectID;
 		todo.heading = heading;
+		todo.when = date
 		todoArray.push(todo)
 	}
 	return todoArray
 }
 
 // Run previously created functions so data can be sent to Things
-var head = headingChooser();
+var head = headingAndDate();
 var todos = todoList(head);
 
 // Create Things JSON container to send todos.
