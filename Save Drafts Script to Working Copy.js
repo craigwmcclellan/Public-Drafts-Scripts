773d03930efc25367b2d875725d28bda4416e4f6
Craig McClellan <craig@craigmcclellan.com>
2018-10-18 17:27:22 -0500

    added explanatory comments

--- Save Drafts Script to Working Copy.js
+++ Save Drafts Script to Working Copy.js
@@ -1,19 +1,25 @@
 // Save Drafts Script to Working Copy
 
+// This script will save the script currently loaded in the Drafts editor as a Javascript file in the Working Copy repo you choose using the Credential method.
+
 var credential = Credential.create("Working Copy", "Working Copy Git Client for iOS");
 
+// Creates credential input for your Working Copy URL scheme key which can be retrieved from the Working Copy settings.
 credential.addTextField("urlKey", "Working Copy URL Key");
+// Credential input for repo name
 credential.addTextField("scriptsRepo", "Scripts Repo");
 
 credential.authorize();
 
 var key = credential.getValue("urlKey");
 var repo = credential.getValue("scriptsRepo");
+// The file name in Working Copy will be set as the title of the Draft without any special characters like comments or markdown headings.
 var safeTitle = draft.processTemplate("[[safe_title]]");
 var path = safeTitle + ".js";
 
-const baseURL = "working-copy://x-callback-url/write/";
 
+// Create callback URL to send data to Working Copy
+const baseURL = "working-copy://x-callback-url/write/";
 
 var cb = CallbackURL.create();
 cb.baseURL = baseURL;
